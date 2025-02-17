fadeb010baedd8e276cc80aab84e8fb1cdda193e
f4eriebambi <d00271879@student.dkit.ie>
2025-02-17 17:55:29 +0000

    reading in lines from my nhlstats2324.csv file and display them on screen [ not in format ]

--- main.cpp
+++ main.cpp
@@ -1,6 +1,75 @@
 #include <iostream>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+
+void readCSV();
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    readCSV();
+}
+
+struct Player {
+    string name;
+    string team;
+    char position;
+    int gamesPlayed;
+    int goals;
+    int points;
+    float pointsPerGamePlayed;
+    string iceTimePerGamePlayed;
+};
+
+void parse(string line, Player &player) {
+    stringstream ss(line);
+    string temp;
+
+    getline(ss, player.name, ',');
+    getline(ss, player.team, ',');
+
+    getline(ss, temp, ',');
+    player.position = temp[0];
+
+    getline(ss, temp, ',');
+    player.gamesPlayed = stoi(temp);
+
+    getline(ss, temp, ',');
+    player.goals = stoi(temp);
+
+    getline(ss, temp, ',');
+    player.points = stoi(temp);
+
+    getline(ss, temp, ',');
+    player.pointsPerGamePlayed = stof(temp);
+
+    // getline(ss, player.iceTimePerGamePlayed);
+    getline(ss, player.iceTimePerGamePlayed, ',');
+}
+
+void readCSV() {
+    ifstream fin("nhlstats2324.csv");
+
+    if (fin) {
+        while (!fin.eof()) {
+            string line;
+            getline(fin, line);
+
+            Player player;
+
+            if (line.length() > 0) {
+                parse(line, player);
+                cout << player.name << ", "
+                << player.team << ", " << player.position << ", "
+                << player.gamesPlayed << ", " << player.goals << ", "
+                << player.points << ", " << player.pointsPerGamePlayed << ", "
+                << player.iceTimePerGamePlayed
+                << endl;
+            }
+        }
+        fin.close();
+    }
+    else {
+        cout << "File not found !" << endl;
+    }
 }
