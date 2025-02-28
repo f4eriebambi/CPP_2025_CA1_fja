e3fd9c7ebad586c636d6adffc65254604583e508
f4eriebambi <d00271879@student.dkit.ie>
2025-02-28 04:47:05 +0000

    - completed stage 4, i now have a console-based menu system that properly loads my functions made in stage 3 - redid 3.2 as i felt like i was ready the question wrong and only returning first instance of searched position so now i am returning all index matches - all my results are clearly display and tables aligned correctly - made inputs from user case-insensitive - realized i made a mistake in my code for stage 3.1 and wasn't displaying first player because i was using "if (iter != players.cbegin())" - fixed issue with cmake-build-debug folder and moved .csv file back there - cleaned up code - handling wrong input type - COMPLETED PROJECT

--- main.cpp
+++ main.cpp
@@ -126,7 +126,7 @@ void parse(string line, Player &player) {
 
     getline(ss, player.iceTimePerGamePlayed);
 }
-void load(string fileName, vector<Player> &players) {
+void load(const string fileName, vector<Player> &players) {
     ifstream fin(fileName);
     if (fin) {
         string line;
@@ -158,7 +158,6 @@ void displayPlayer(const Player &player) {
 void display(const vector<Player> &players) {
     vector<Player>::const_iterator iter;
     for (iter = players.cbegin(); iter != players.cend(); iter++) {
-        if (iter != players.cbegin()) {
             cout << left
             << setw(24) << iter->name
             << setw(10) << iter->team
@@ -169,18 +168,27 @@ void display(const vector<Player> &players) {
             << setw(20) << fixed << setprecision(2) << iter->pointsPerGamePlayed
             << setw(20) << iter->iceTimePerGamePlayed
             << endl;
-        }
     }
 }
 
 // ---- STAGE 3.2 ----
-int searchPosition(const vector<Player> &players, char position) {
+// int searchPosition(const vector<Player> &players, char position) { // ONLY GETTING FIRST INDEX MATCH OF SEARCHED HOCKEY POSITION ::old code
+//     for (int i = 0; i < players.size(); i++) {
+//         if (players[i].position == position) {
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
+// ---- STAGE 3.2 ----
+vector<int> searchPosition(const vector<Player> &players, char position) { // GETTING ALL INDEX MATCHES OF SEARCHED HOCKEY POSITION
+    vector<int> positionMatches;
     for (int i = 0; i < players.size(); i++) {
         if (players[i].position == position) {
-            return i;
+            positionMatches.push_back(i);
         }
     }
-    return -1;
+    return positionMatches;
 }
 
 // ---- STAGE 3.3 ----
@@ -197,6 +205,11 @@ void displayPlayerByTeam(const vector<Player> &players, const string &team) {
     bool found = false;
     for (const Player &player : players) {
         if (player.team == team) {
+            if (!found) {
+                cout << "- Player(s) of " << team << " ::";
+                headers();
+                found = true;
+            }
             cout << left
             << setw(24) << player.name
             << setw(10) << player.team
@@ -234,11 +247,12 @@ int highLowAvgGoals(const vector<Player> &players, Player &highestScorer, Player
     double average = total / players.size();
     int averageGoal = (int)round(average);
 
-    cout << "Highest Number of Goals made in 23/24 Season :: " << max<< " goals made by "
+    cout << "\n========== NHL PLAYER STATS 23/24 SEASON ==========\n";
+    cout << "Highest Number of Goals :: " << max << " goals made by "
     << highestScorer.name << endl;
-    cout << "Lowest Number of Goals made in 23/24 Season :: " << min << " goal(s) made by "
+    cout << "Lowest Number of Goals :: " << min << " goal(s) made by "
     << lowestScorer.name << endl;
-    cout << "Average Goals Scored in 23/24 Season :: " << averageGoal << endl;
+    cout << "Average Goals Scored :: " << averageGoal << endl;
 
     return averageGoal;
 }
@@ -246,16 +260,20 @@ int highLowAvgGoals(const vector<Player> &players, Player &highestScorer, Player
 // ---- STAGE 3.6 ----
 list<Player> searchPlayerName(const vector<Player> &players, const string &search) {
     list<Player> pList;
-    string searchUpper = search;
-    for (char &c : searchUpper) {
-        c = toupper(c);
+    string searchUpper;
+    for (char c : search) {
+        if (!isspace(c)) { // https://www.scaler.com/topics/removing-whitespace-from-a-string-in-cpp/
+            searchUpper.push_back(toupper(c));
+        }
     }
 
     vector<Player>::const_iterator iter;
     for (iter = players.cbegin(); iter != players.cend(); iter++) {
-        string nameUpper = iter->name;
-        for (char &c : nameUpper) {
-            c = toupper(c);
+        string nameUpper;
+        for (char c : iter->name) {
+            if (!isspace(c)) {
+                nameUpper.push_back(toupper(c));
+            }
         }
 
         if (nameUpper.find(searchUpper) != string::npos) { // https://stackoverflow.com/questions/29461786/partial-string-search-in-c
@@ -275,7 +293,7 @@ void printMenu() {
     cout << " ============ NHL PLayer Stats Menu =============\n";
     cout << "--------------------------------------------------\n";
     cout << "1. Display All Players____.\n";
-    cout << "2. Search Player by Position____.\n";
+    cout << "2. Search Players by Position____.\n";
     cout << "3. Count Players on Teams____.\n";
     cout << "4. Display Players of Team____.\n";
     cout << "5. Highest+Lowest Number of Goals & Average Goals of 23/24____.\n";
@@ -301,11 +319,19 @@ int main() {
         displayPlayer(p[i]);
     }
 
+
     // ---- MENU ----
     int choice = 0;
     while (choice != 8) {
         printMenu();
-        cin >> choice;
+        // cin >> choice;
+
+        if (!(cin >> choice)) { // https://cplusplus.com/forum/beginner/142433/
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cout << "INVALID OPTION - Try again ><" << endl;
+            continue;
+        }
         cin.ignore();
 
         switch (choice) {
@@ -319,27 +345,53 @@ int main() {
             // ---- STAGE 3.2 ----
             case 2: {
                 cout << "\n[ .. SEARCH PLAYER BY POSITION ]\n";
-                cout << "ENTER THE HOCKEY POSITION YOU WOULD LIKE TO SEE PLAYER OF (C/D/L/R) : \n";
+                cout << "Enter the hockey position you would like to see players of - Enter C, D, L or R : \n";
                 char position;
                 cin >> position;
+                cin.ignore(1000, '\n');
                 position = toupper(position); // https://www.programiz.com/cpp-programming/library-function/cctype/toupper
-                int index = searchPosition(p, position);
-                if (index == -1) {
-                    cout << "!! NO PLAYER(S) FOUND AT THE INDEX !!" << endl;
+                // int index = searchPosition(p, position); // ONLY GETTING FIRST INDEX MATCH OF SEARCHED HOCKEY POSITION ::old code
+                // if (index != -1) {
+                //     cout << "- Player(s) found with position '" << position << "'";
+                //     headers();
+                //     cout << "index " << index << " ::\n";
+                //     cout << left
+                //     << setw(24) << p[index].name
+                //     << setw(10) << p[index].team
+                //     << setw(10) << p[index].position
+                //     << setw(15) << p[index].gamesPlayed
+                //     << setw(10) << p[index].goals
+                //     << setw(10) << p[index].points
+                //     << setw(20) << fixed << setprecision(2) << p[index].pointsPerGamePlayed
+                //     << setw(20) << p[index].iceTimePerGamePlayed
+                //     << endl;
+                // }
+                // else {
+                //     cout << "!! NO INDEX(ES) OF PLAYER(S) WITH POSITION '" << position << "' !!" << endl;
+                // }
+                // break;
+
+                vector<int> positionMatches = searchPosition(p, position); // GETTING ALL INDEX MATCHES OF SEARCHED HOCKEY POSITION
+                if (!positionMatches.empty()) {
+                    cout << left
+                    << "- Player(s) found with position '" << position << "'";
+                    headers();
+                    for (int index: positionMatches) {
+                        cout << left
+                        << "index " << index << " ::\n"
+                        << setw(24) << p[index].name
+                        << setw(10) << p[index].team
+                        << setw(10) << p[index].position
+                        << setw(15) << p[index].gamesPlayed
+                        << setw(10) << p[index].goals
+                        << setw(10) << p[index].points
+                        << setw(20) << fixed << setprecision(2) << p[index].pointsPerGamePlayed
+                        << setw(20) << p[index].iceTimePerGamePlayed
+                        << "\n" << endl;
+                    }
                 }
                 else {
-                    headers();
-                    cout << "- PLAYER FOUND AT INDEX " << index << " ::\n" << endl;
-                    cout << left
-                    << setw(24) << p[index].name
-                    << setw(10) << p[index].team
-                    << setw(10) << p[index].position
-                    << setw(15) << p[index].gamesPlayed
-                    << setw(10) << p[index].goals
-                    << setw(10) << p[index].points
-                    << setw(20) << fixed << setprecision(2) << p[index].pointsPerGamePlayed
-                    << setw(20) << p[index].iceTimePerGamePlayed
-                    << endl;
+                    cout << "!! NO INDEX(ES) OF PLAYER(S) WITH POSITION '" << position << "' !!" << endl;
                 }
                 break;
             }
@@ -355,23 +407,22 @@ int main() {
                 map<string,int> countPlayers = countTeamPlayerRows(p);
                 for (const auto &teamCount : countPlayers) {
                     cout << left
+                    // https://www.geeksforgeeks.org/pair-in-cpp-stl/
                     << setw(24) << teamCount.first
                     << setw(10) << teamCount.second
-                    << endl; // https://www.geeksforgeeks.org/pair-in-cpp-stl/
+                    << endl;
                 }
                 break;
             }
             // ---- STAGE 3.4 ----
             case 4: {
                 cout << "\n[ ..DISPLAY PLAYERS OF TEAM ]\n";
-                cout << "ENTER A TEAM NAME (3 letter format) : \n";
+                cout << "Enter a team name (3 letter format) : \n";
                 string team;
                 cin >> team;
                 for (char &c : team) {
                     c = toupper(c);
                 }
-                headers();
-                cout << "- PLAYER(S) OF " << team << " ::\n\n";
                 displayPlayerByTeam(p, team);
                 break;
             }
@@ -386,16 +437,14 @@ int main() {
             // ---- STAGE 3.6 ----
             case 6: {
                 cout << "\n[ ..SEARCH PLAYER BY NAME ]\n";
-                cout << "ENTER NAME TO SEARCH PLAYER(S) : \n";
+                cout << "Enter name to search player(s) : \n";
                 string search;
-                cin >> search;
-                // for (char &c : search) {
-                //     c = toupper(c);
-                // }
+                getline(cin, search);
+
                 list<Player> searchNames = searchPlayerName(p, search);
                 if (!searchNames.empty()) {
+                    cout << "- Player(s) found with name containing '" << search << "' ::";
                     headers();
-                    cout << "- PLAYER(S) FOUND WITH A NAME CONTAINING '" << search << "' ::\n" << endl;
                     for (const Player &player : searchNames) {
                         displayPlayer(player);
                     }
@@ -407,11 +456,11 @@ int main() {
             }
             // ---- STAGE 3.7 ----
             case 7: {
-                cout << "\n[ ..PLAYERS SORTED BY 'Points Per Game' IN DESCENDING ORDER ]";
+                cout << "\n[ ..PLAYERS SORTED BY 'Points Per Game' IN DESCENDING ORDER ]\n";
                 vector <Player> pDesc = p;
                 sort(pDesc.begin(), pDesc.end(), descendingPointsPerGame);
+                cout << "\n- Players sorted by 'Points Per Game' in descending order ::";
                 headers();
-                cout << "- PLAYERS SORTED BY 'Points Per Game' IN DESCENDING ORDER ::\n" << endl;
                 display(pDesc);
                 break;
             }
